d10984081bbf7436e0192b51f8f43eb3bb1351c4
milesbythesea <keithmilesbr@icloud.com>
2025-08-23 18:18:08 -0500

    Fix custom domain configuration for baggymagnum.com

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="coming-soon-container">
-        <img src={process.env.PUBLIC_URL + "/bm-soon.jpg"} className="coming-soon-image" alt="Coming Soon" />
+        <img src="/bm-soon.jpg" className="coming-soon-image" alt="Coming Soon" />
         <h1 className="coming-soon-text">Coming Soon</h1>
       </div>
     </div>
